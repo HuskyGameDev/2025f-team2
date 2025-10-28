91648d4c23447a7f97eec11b988d174e4b56bb57
Ethan Emerick <efemeric@mtu.edu>
2025-10-28 01:41:39 -0400

    Level Select Screen
    Added a level select screen. Also added a button for it in the menu. Added a template for level select buttons. Reworked "PlayGame" script to be more versatile for future buttons.

---
+++ Scripts/UI_Scripts/OtherSceneButton.gd
@@ -0,0 +1,9 @@
+extends Button
+
+#set this var to whatever scene you want this button to send you to
+@export var targetScene: String = "res://Scenes/TestScenes/TestScene.tscn";
+
+#When the button is pressed, loads the next scene
+func _on_pressed() -> void:
+	print("A")
+	get_tree().change_scene_to_file(targetScene)
--- Scripts/UI_Scripts/Play_Game.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-#When the "Play-Game" button is pressed, loads the next scene
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/TestScenes/TestScene.tscn")
