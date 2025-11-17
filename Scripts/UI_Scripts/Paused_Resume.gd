44e84d00ea9169146f518bea345955f8f10f6cbd
CreepyCatMeow <creepycatgames@gmail.com>
2025-11-17 17:10:17 -0500

    UI changes

--- Scripts/UI_Scripts/Paused_Resume.gd
+++ Scripts/UI_Scripts/Paused_Resume.gd
@@ -2,6 +2,6 @@ extends Button
 
 #This script gives functionality to the "Resume" button in the pause menu
 func _on_pressed() -> void:
-	get_parent().get_parent()._pausePressed()
-	get_parent().get_parent().visible=false
+	get_parent()._pausePressed()
+	get_parent().visible=false
 	get_tree().paused = false
